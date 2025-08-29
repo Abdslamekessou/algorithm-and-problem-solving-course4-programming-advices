8e45745f3cd6f00e06a85f0d9e074ca11a6a515f
Abdslamekessou <151072580+Abdslamekessou@users.noreply.github.com>
2025-08-29 18:11:58 +0100

    Create Problem043.cpp

---
+++ Problem043.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <math.h>
+
+// ⏱️ Program to read a positive number of seconds  
+// 🔄 Then convert it into days, hours, minutes, and seconds
+
+using namespace std;
+
+
+struct stTaskDuration{
+  int NumberOfDays , NumberOfHours , NumberOfMinutes , NumberOfSecondes;
+};
+
+
+
+int ReadPositiveNumber(string Message){
+
+  int Number;
+
+  do{
+     cout << Message << endl;
+     cin >> Number;
+  }while(Number <= 0);
+
+  return Number;
+
+}
+
+
+
+stTaskDuration SecondesToTaskDuration(int TotalSecondes){
+
+  stTaskDuration TaskDuration;
+
+  const int SecondesPerDays = 24 * 60 * 60;
+  const int SecondesPerHours = 60 * 60;
+  const int SecondesPerMinutes = 60;
+
+  int Remainder = 0;
+
+  TaskDuration.NumberOfDays = floor(TotalSecondes / SecondesPerDays);
+  Remainder = TotalSecondes % SecondesPerDays;
+
+  TaskDuration.NumberOfHours = floor(Remainder / SecondesPerHours);
+  Remainder = Remainder % SecondesPerHours;
+
+  TaskDuration.NumberOfMinutes = floor(Remainder / SecondesPerMinutes);
+  Remainder = Remainder % SecondesPerMinutes;
+
+  TaskDuration.NumberOfSecondes = Remainder;
+
+  return TaskDuration;
+
+}
+
+
+void PrintTaskDurationDetails(stTaskDuration TaskDuration){
+
+   cout << TaskDuration.NumberOfDays << " : " << TaskDuration.NumberOfHours << " : "
+        << TaskDuration.NumberOfMinutes << " : " << TaskDuration.NumberOfSecondes <<endl;
+
+}
+
+
+
+int main()
+{
+    
+    PrintTaskDurationDetails(SecondesToTaskDuration(ReadPositiveNumber("Please Enter Number Of Secondes? ")));
+
+    return 0;
+}
